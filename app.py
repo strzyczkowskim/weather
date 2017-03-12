cc933db5a10f835dd1d44dc80170efacbb397d17
Maciej Strzyczkowski <maciejstrzyczkowski@MacBook-Pro-Maciej.local>
2017-03-12 13:06:04 +0100

    app.py change2

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def makeWebhookResult(data):
     # print(json.dumps(item, indent=4))
 
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + int((condition.get('temp') - 32)/1.8) + " " + units.get('temperature')
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 
     print("Response:")
     print(speech)
