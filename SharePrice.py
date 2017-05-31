5b5d09389559058d938166a7ed02015ea8c21220
Maciej Strzyczkowski <strzyczkowskim@users.noreply.github.com>
2017-05-31 13:17:22 +0200

    Delete SharePrice.py

--- SharePrice.py
+++
@@ -1,8 +0,0 @@
-import ystockquote
-
-class SharePrice:
-    def __init__(self ,enterpriseCode):
-        self.price = ystockquote.get_today_open(enterpriseCode)
-
-    def showAnswer(self):
-        return "This is the current price: " , self.price
